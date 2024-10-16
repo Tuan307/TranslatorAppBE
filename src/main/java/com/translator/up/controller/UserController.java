48cda7ad45989fa973bbcf014df9b518c2537b0c
Pham Tuan <tuanprokt44@gmail.com>
2024-10-16 23:57:10 +0700

    add session authentication before do api job and add get profile and edit profile api

--- src/main/java/com/translator/up/controller/UserController.java
+++ src/main/java/com/translator/up/controller/UserController.java
@@ -5,6 +5,7 @@ import com.translator.up.model.common.ApiResponse;
 import com.translator.up.model.request.LoginRequest;
 import com.translator.up.model.request.ProjectRequest;
 import com.translator.up.model.request.RegisterUserRequest;
+import com.translator.up.model.request.UpdateUserRequest;
 import com.translator.up.model.response.ProjectDTO;
 import com.translator.up.service.user.UserService;
 import jakarta.servlet.http.HttpSession;
@@ -42,6 +43,24 @@ public class UserController {
         }
     }
 
+    @GetMapping("/profile")
+    public ApiResponse<UserEntity> getUserProfile(@RequestParam("userId") Long userId, HttpSession session) {
+        UserEntity user = (UserEntity) session.getAttribute("user");
+        if (user == null) {
+            return new ApiResponse<>("error", "Login required", null, "401");
+        }
+        return userService.getUserProfile(userId);
+    }
+
+    @PutMapping("/profile/edit")
+    public ApiResponse<UserEntity> editUserProfile(@RequestBody UpdateUserRequest request, HttpSession session) {
+        UserEntity user = (UserEntity) session.getAttribute("user");
+        if (user == null) {
+            return new ApiResponse<>("error", "Login required", null, "401");
+        }
+        return userService.editUserProfile(request);
+    }
+
     @PostMapping("/logout")
     public ApiResponse<String> logout(HttpSession session) {
         session.invalidate();
@@ -49,8 +68,11 @@ public class UserController {
     }
 
     @PostMapping("/create/translaterequest")
-    public ApiResponse<ProjectDTO> createTranslateRequest(@RequestBody ProjectRequest request) {
+    public ApiResponse<ProjectDTO> createTranslateRequest(@RequestBody ProjectRequest request, HttpSession httpSession) {
+        UserEntity user = (UserEntity) httpSession.getAttribute("user");
+        if (user == null) {
+            return new ApiResponse<>("error", "Login required", null, "401");
+        }
         return userService.addProject(request);
     }
-
 }
---
+++ src/main/java/com/translator/up/model/request/UpdateUserRequest.java
@@ -0,0 +1,17 @@
+package com.translator.up.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UpdateUserRequest {
+    private Long id;
+    private String fullName;
+    private String phoneNumber;
+    private String password;
+}
