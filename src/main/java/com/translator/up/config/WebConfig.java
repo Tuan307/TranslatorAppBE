dc47ba9d8657b2fb86fa36e8c52e547dd2d7abc9
quangdongoc <doquang227@gmail.com>
2024-10-20 22:41:51 +0700

    add cor config

---
+++ src/main/java/com/translator/up/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.translator.up.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // Cho phép React app ở localhost:3000
+                .allowedMethods("GET", "POST") // Các phương thức HTTP cho phép
+                .allowCredentials(true); // Cho phép gửi cookie
+    }
+}
